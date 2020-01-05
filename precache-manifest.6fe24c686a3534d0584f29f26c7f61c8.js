7efbe9c84a6550f5ac9b48d8dfe9be8aa1db884c
Eraldo Forgoli <eraldoforgoli@gmail.com>
2020-01-05 18:50:11 +0100

    Updates

---
+++ precache-manifest.6fe24c686a3534d0584f29f26c7f61c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8491a4e1c0777bae045585969519d71",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "e876c669e19dbf5edd51",
+    "url": "/personal-website/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "349426a3877a7df2266e",
+    "url": "/personal-website/static/css/main.14ec28b5.chunk.css"
+  },
+  {
+    "revision": "e876c669e19dbf5edd51",
+    "url": "/personal-website/static/js/2.db26341b.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/personal-website/static/js/2.db26341b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2650b384206295c1b7aa",
+    "url": "/personal-website/static/js/3.671d6ae6.chunk.js"
+  },
+  {
+    "revision": "284f0a8d31bbacbb4231",
+    "url": "/personal-website/static/js/4.88a637b8.chunk.js"
+  },
+  {
+    "revision": "31a15cf7a7db7530e05f",
+    "url": "/personal-website/static/js/5.186954b4.chunk.js"
+  },
+  {
+    "revision": "349426a3877a7df2266e",
+    "url": "/personal-website/static/js/main.0fb6ca47.chunk.js"
+  },
+  {
+    "revision": "eb28d3b07c16e73d1442",
+    "url": "/personal-website/static/js/runtime-main.101b074d.js"
+  }
+]);
