2609082e06405d62d4f3ed8aad63fbf8aaa3c509
Eraldo Forgoli <eraldoforgoli@gmail.com>
2020-01-05 18:55:20 +0100

    Updates

---
+++ precache-manifest.67a409ec31b62b04882e8f60688d01fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49e611046ab63d5d36d2206f5cf7899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd608e48e6b0f6ca125a",
+    "url": "/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "f080c03db6e0339754af",
+    "url": "/static/css/main.14ec28b5.chunk.css"
+  },
+  {
+    "revision": "dd608e48e6b0f6ca125a",
+    "url": "/static/js/2.cebe30af.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.cebe30af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2650b384206295c1b7aa",
+    "url": "/static/js/3.671d6ae6.chunk.js"
+  },
+  {
+    "revision": "284f0a8d31bbacbb4231",
+    "url": "/static/js/4.88a637b8.chunk.js"
+  },
+  {
+    "revision": "31a15cf7a7db7530e05f",
+    "url": "/static/js/5.186954b4.chunk.js"
+  },
+  {
+    "revision": "f080c03db6e0339754af",
+    "url": "/static/js/main.52a0c3be.chunk.js"
+  },
+  {
+    "revision": "5f4f326469b791b5c13a",
+    "url": "/static/js/runtime-main.ad43a16a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.6fe24c686a3534d0584f29f26c7f61c8.js"
+  "/precache-manifest.67a409ec31b62b04882e8f60688d01fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
